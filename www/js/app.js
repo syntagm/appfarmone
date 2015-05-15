62c7e1d3a193946d1588c35907251bdb97bd0854
syntagm <syntagm@icon.co.za>
2015-05-15 13:34:02 +0200

    appload 05
    app.js changes

--- www/js/app.js
+++ www/js/app.js
@@ -23,31 +23,33 @@ angular.module('starter', ['ionic'])
 // retrieving Angular modules 
 //' starter' is the name of this angular module example (also set in 
 // a < body > attribute in index.html) 
-// the 2nd parameter is an array of 'requires' 
-angular.module(' starter', [' ionic']) 
+// the 2nd parameter is an array of 
+'requires' 
+angular.module('starter', 
+['ionic']) 
 .run( function ($ionicPlatform) { 
 $ionicPlatform.ready( function 
 () { 
 
-function makeListItem( textStr) { return '< li class =" item" >' + textStr + '</ li >'; } 
+function
+makeListItem( textStr) { 
+return '<li class ="item">' + textStr + '</li>'; 
+} 
 
 // Hide the accessory bar by default (remove this to show the 
 // accessory bar above the keyboard for form inputs) 
 
 if (window.cordova && 
-window.cordova.plugins.Keyboard)
- { 
- 
-cordova.plugins.Keyboard.hideKeyboardAccessoryBar( true); 
+window.cordova.plugins.Keyboard) {  
+cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true); 
  } 
- if (window.StatusBar) { StatusBar.styleDefault();
-  } 
+ if (window.StatusBar) {StatusBar.styleDefault();
+ } 
   
-console.log(" Cordova: " + device.cordova);
+console.log("Cordova: " + device.cordova);
 var tmpStr; 
 tmpStr = '< ul class =" list" >'; 
-tmpStr += 
-makeListItem('Cordova Version: '
+tmpStr += makeListItem('Cordova Version: '
 + device.cordova); 
 tmpStr += makeListItem('Operating System: ' 
 + device.platform); 
@@ -65,7 +67,7 @@ var element = document.getElementById(' deviceInfo');
 // running the application 
 
 element.innerHTML = tmpStr; 
-}); 
+ }); 
 });
 
 

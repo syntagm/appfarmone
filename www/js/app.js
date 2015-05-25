cf0d25a196e97040b35a010241fb57d42d2e734f
syntagm <syntagm@icon.co.za>
2015-05-25 19:28:30 +0200

    appload 15
    catchup

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,25 @@
-angular.module('twitterFeed', ['ionic', 'twitterFeed.controllers', 'twitterFeed.services', 'twitterFeed.filters'])
+// Ionic Starter App
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var example = angular.module('starter', ['ionic', 'ngSanitize']);
+
+example.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
+
+example.controller("ExampleController", function($scope) {
+    $scope.myHTML = 'Check out my programming <a href="http://www.appfarm.co.za">blog</a> while you are here';
+});
+
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
